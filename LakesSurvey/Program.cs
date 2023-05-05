ebdb152a2cc3fbbe9a139ed613fabdf6f209ea33
cavanfarrell <cavan.farrell@icloud.com>
2023-05-04 21:49:59 -0500

    parallel.foreachasyncs the thing

--- LakesSurvey/Program.cs
+++ LakesSurvey/Program.cs
@@ -1,54 +1,5 @@
 ﻿using LakesSurvey;
 using LakesSurveyModels.Models;
 
-var apiClient = new DnrApiClient();
-var dataMapper = new DnrLakesDataMapper();
-
-await dataMapper.LoadFishTypes();
-
-await dataMapper.LoadLakes();
-
-var lakeList = dataMapper.GetLakes();
-
-foreach (var lakeId in lakeList)
-{
-     Console.WriteLine($"Getting survey for {lakeId.LakeId} ({lakeId.LakeName}), {lakeId.County} County...");
-     try
-     {
-         var surveyResult = await apiClient.GetSurveyResponse(lakeId.LakeId);
-         if (surveyResult.Status != "SUCCESS" && !(lakeId.Survey > 0))
-         {
-             await dataMapper.UpdateLakeList(lakeId, -1);
-             continue;
-         }
-
-         var surveyId = await dataMapper.LoadSurveyResult(surveyResult, lakeId);
-         
-         if (surveyId > 0)
-         {
-             await dataMapper.UpdateLakeList(lakeId, surveyId);
-         }
-     }
-     catch
-     {
-         await dataMapper.UpdateLakeList(lakeId, -2);
-         Console.WriteLine($"Unable to get data for {lakeId.LakeId} ({lakeId.LakeName})");
-     }
-}
-
-
-// var badCoordinatesLakes = lakeList.Where(l => l.Distance > 1000 && l.Survey > 0);
-
-// foreach (var lake in badCoordinatesLakes)
-// {
-//     var latitude = Double.Parse(lake.Coordinates.Split(", ")[1]);
-//     var longitude = Double.Parse(lake.Coordinates.Split(", ")[0]);
-//     var fixedDistance = apiClient.GetDistance(longitude, latitude);
-//
-//     if (Math.Abs(lake.Distance - fixedDistance) > .001)
-//     {
-//         lake.Distance = fixedDistance;
-//         Console.WriteLine($"{lake.LakeName} - {fixedDistance}");
-//         await dataMapper.UpdateLakeList(lake, lake.Survey);
-//     }
-// }
+var surveyProcessor = new SurveyProcessor();
+await surveyProcessor.ProcessLakesParallel();
---
+++ LakesSurvey/SurveyProcessor.cs
@@ -0,0 +1,104 @@
+namespace LakesSurvey;
+
+public class SurveyProcessor
+{
+    private DnrApiClient _apiClient;
+    private DateTime startDate;
+    private DateTime endDate;
+    
+    public SurveyProcessor()
+    {
+        _apiClient = new DnrApiClient();
+    }
+
+    public async Task ProcessLakesParallel() // 00:02:43.1176670
+    {
+        startDate = DateTime.Now;
+
+        var dataMapper = new DnrLakesDataMapper();
+        await dataMapper.LoadFishTypes();
+
+        await dataMapper.LoadLakes();
+
+        var lakeList = dataMapper.GetLakes();
+        
+        var options = new ParallelOptions()
+        {
+            MaxDegreeOfParallelism = 20
+        };
+
+        await Parallel.ForEachAsync(lakeList, options , async (lakeId, ct) =>
+        {
+            var tDataMapper = new DnrLakesDataMapper();
+            Console.WriteLine($"Getting survey for {lakeId.LakeId} ({lakeId.LakeName}), {lakeId.County} County...");
+            try
+            {
+                var surveyResult = await _apiClient.GetSurveyResponse(lakeId.LakeId);
+                if (surveyResult.Status != "SUCCESS" && !(lakeId.Survey > 0))
+                {
+                    await tDataMapper.UpdateLakeList(lakeId, -1);
+                    return;
+                }
+
+                var surveyId = await tDataMapper.LoadSurveyResult(surveyResult, lakeId);
+
+                if (surveyId > 0)
+                {
+                    await tDataMapper.UpdateLakeList(lakeId, surveyId);
+                }
+            }
+            catch
+            {
+                await tDataMapper.UpdateLakeList(lakeId, -2);
+                Console.WriteLine($"Unable to get data for {lakeId.LakeId} ({lakeId.LakeName})");
+            }
+        });
+        
+        endDate = DateTime.Now;
+        
+        Console.WriteLine(endDate - startDate);
+    }
+    
+
+    public async Task ProcessLakes() // 00:30:58.6664180
+    {
+        startDate = DateTime.Now;
+
+        var dataMapper = new DnrLakesDataMapper();
+        await dataMapper.LoadFishTypes();
+
+        await dataMapper.LoadLakes();
+
+        var lakeList = dataMapper.GetLakes();
+        
+        foreach (var lakeId in lakeList)
+        {
+            Console.WriteLine($"Getting survey for {lakeId.LakeId} ({lakeId.LakeName}), {lakeId.County} County...");
+            try
+            {
+                var surveyResult = await _apiClient.GetSurveyResponse(lakeId.LakeId);
+                if (surveyResult.Status != "SUCCESS" && !(lakeId.Survey > 0))
+                {
+                    await dataMapper.UpdateLakeList(lakeId, -1);
+                    return;
+                }
+
+                var surveyId = await dataMapper.LoadSurveyResult(surveyResult, lakeId);
+
+                if (surveyId > 0)
+                {
+                    await dataMapper.UpdateLakeList(lakeId, surveyId);
+                }
+            }
+            catch
+            {
+                await dataMapper.UpdateLakeList(lakeId, -2);
+                Console.WriteLine($"Unable to get data for {lakeId.LakeId} ({lakeId.LakeName})");
+            }
+        }
+        
+        endDate = DateTime.Now;
+        
+        Console.WriteLine(endDate - startDate);
+    }
+}
